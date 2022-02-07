bbeaa1d03090f1ea1820b18a53fa24bdf4d38654
4kawki <13zolw13@gmail.com>
2022-02-07 17:30:37 +0100

    fixed error handling from  omdb api service

--- movies/src/services/omdbApi.services.ts
+++ movies/src/services/omdbApi.services.ts
@@ -6,7 +6,7 @@ import {
 } from '../models/movie.models';
 import log from '../utils/logger';
 
-export async function getMovie(title: string) {
+export async function getMovie(title: string): Promise<MovieDetails | null> {
     const apikey = config.get("omdb_key");
 
     const apiUrl = config.get("apiUrl");
@@ -15,20 +15,26 @@ export async function getMovie(title: string) {
         const {
             data: movieInfo
         } = await axios.get(url)
-        const movie: MovieDetails = {
-            Title: movieInfo.Title,
-            Actors: movieInfo.Actors,
-            Director: movieInfo.Director,
-            Genre: movieInfo.Genre,
-            Released: movieInfo.Released,
-            Plot: movieInfo.Plot,
-            Runtime: movieInfo.Runtime,
-            Awards: movieInfo.Awards
-        };
-        log.info(movie, 'movie movieinput');
-        return movie;
-    } catch (error) {
+
+        if (movieInfo.Response !== "False") {
+            log.info(movieInfo, 'movie info from axios')
+            const movie: MovieDetails = {
+                Title: movieInfo.Title,
+                Actors: movieInfo.Actors,
+                Director: movieInfo.Director,
+                Genre: movieInfo.Genre,
+                Released: movieInfo.Released,
+                Plot: movieInfo.Plot,
+                Runtime: movieInfo.Runtime,
+                Awards: movieInfo.Awards
+            };
+            log.info(movie, 'movie movieinput');
+
+            return movie;
+        }
+        return null;
+    } catch (error: any) {
         log.error(error);
-        return;
+        return null;
     }
 }
