972e8f930b1fcfd4ffeb7f2231a42e1e4f16da58
tomek <tomaszpawelkowalski@gmail.com>
2022-05-02 21:08:13 +0200

    refactor checkEnvVar

--- movies/src/utils/checkingEnvVar.ts
+++ movies/src/utils/checkingEnvVar.ts
@@ -1,35 +1,23 @@
 import config from 'config';
 import log from './logger';
-function checkEnvVar() {
-    
-    if (!config.has('port')) {
-        log.error('No port variable');
-    }
-    if (!config.has('dbUri')) {
-        log.error('No dbUri variable');
-    }
-    // if (!config.has('cookieKey')) {
-    //     log.error('No cookieKey variable');
-    // }
-    if (!config.has('omdb_key')) {
-        log.error('No omdb_key variable');
-    }
-    if (!config.has('apiUrl')) {
-        log.error('No omdb_key variable');
-    }
-    if (!config.has('level')) {
-        log.error('No level variable');
-    }
-    if (!config.has('limitBasic')) {
-        log.error('No limitBasic variable');
-    }
-    if (!config.has('authServiceURL')) {
-        log.error('No authServiceURL variable');
-    }
-    if (!config.has('jwt')) {
-        log.error('No  jwt key variable');
-    }
+ const ArrayOfEnvVar = [
+		"dbUri",
+		"jwt",
+		"level",
+		"limitBasic",
+		"authServiceURL",
+		"omdb_key",
+		"apiUrl",
+ ];
+ export default function checkEnvVar() {
+	ArrayOfEnvVar.forEach((envVar) => {
+		if (!config.has(envVar)) {
+			throw new Error(`${envVar} is not defined`);
+		} else {
+			log.info(`${envVar} is defined`);
+		}
+	});
 }
+ 
+ 
 
-
-export default checkEnvVar;
