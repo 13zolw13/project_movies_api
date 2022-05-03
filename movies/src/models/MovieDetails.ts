6934c1923dc8d59f63cd4c8f84776ecb407d8e48
tomek <tomaszpawelkowalski@gmail.com>
2022-05-03 14:08:06 +0200

    chore: new file for interafce  movie details

---
+++ movies/src/models/MovieDetails.ts
@@ -0,0 +1,11 @@
+
+export interface MovieDetails {
+    Title: string;
+    Actors: string;
+    Director: string;
+    Genre: string;
+    Released: string;
+    Plot: string;
+    Runtime: string;
+    Awards: string;
+}
